8d575dc2eb1c1852873a5ad9fdb6b631e9186967
aurelieglassey <aurelie.glassey@students.hevs.ch>
2019-03-13 09:31:24 +0100

    Entity and models added

---
+++ app/src/main/java/hevs/it/SkiRunV2/entity/UserEntity.java
@@ -0,0 +1,96 @@
+package hevs.it.SkiRunV2.entity;
+
+import android.support.annotation.NonNull;
+
+import com.google.firebase.database.Exclude;
+
+import hevs.it.SkiRunV2.models.User;
+
+
+public class UserEntity implements User {
+
+    @NonNull
+    @Exclude
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private String role;
+    private String club;
+    private String jobPreference;
+
+
+    public UserEntity(){}
+
+    public UserEntity(User user){
+        this.email = user.getEmail();
+        this.firstname = user.getFirstname();
+        this.lastname = user.getLastname();
+        this.phone = user.getPhone();
+        this.role = user.getRole();
+        this.club = user.getClub();
+        this.jobPreference = user.getJobPreference();
+
+
+    }
+
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    @Override
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+
+    @Override
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    @Override
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public String getJobPreference() {
+        return jobPreference;
+    }
+
+    @Override
+    public String getClub() {
+        return club;
+    }
+
+    public void setRole(String role){
+        this.role = role;
+    }
+
+    public String toString(){
+        return getFirstname() + " " + getLastname();
+    }
+}
---
+++ app/src/main/java/hevs/it/SkiRunV2/models/Discipline.java
@@ -0,0 +1,17 @@
+package hevs.it.SkiRunV2.models;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Mike Jason on 13.07.2018.
+ *
+ * Discipline Model
+ *
+ */
+
+public interface Discipline {
+    String getName();
+    Map<String, Object> getMissions();
+
+}
