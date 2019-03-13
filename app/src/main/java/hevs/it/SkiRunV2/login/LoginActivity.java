ca49cad7b308d5db7cfdcb551834a1be41958f62
aurelieglassey <aurelie.glassey@students.hevs.ch>
2019-03-13 13:42:43 +0100

    Application works

--- app/src/main/java/hevs/it/SkiRunV2/login/LoginActivity.java
+++ app/src/main/java/hevs/it/SkiRunV2/login/LoginActivity.java
@@ -1,10 +1,11 @@
-package hevs.it.SkiRunV2;
+package hevs.it.SkiRunV2.login;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
