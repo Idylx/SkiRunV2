947d5780720bf4a2e3aba66726bab1c8cbfcb4a1
aurelieglassey <aurelie.glassey@students.hevs.ch>
2019-03-18 09:04:04 +0100

    Application works

--- app/src/main/java/hevs/it/SkiRunV2/login/LoginActivity.java
+++ app/src/main/java/hevs/it/SkiRunV2/login/LoginActivity.java
@@ -11,23 +11,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
 import hevs.it.SkiRunV2.MainActivity;
 import hevs.it.SkiRunV2.R;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import hevs.it.SkiRunV2.entity.ClubEntity;
 import hevs.it.SkiRunV2.firebase.FirebaseCallBack;
 import hevs.it.SkiRunV2.firebase.FirebaseManager;
-import hevs.it.SkiRunV2.models.Club;
-
 
 public class LoginActivity extends AppCompatActivity {
 
