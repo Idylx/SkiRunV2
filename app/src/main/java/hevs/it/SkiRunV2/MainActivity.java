626ed1cb1a51dc5d840d735216c090f36f91de88
oli <olivier.arbellay@students.hevs.ch>
2019-03-20 14:39:40 +0100

    fragment transaction order done

--- app/src/main/java/hevs/it/SkiRunV2/MainActivity.java
+++ app/src/main/java/hevs/it/SkiRunV2/MainActivity.java
@@ -31,18 +31,25 @@ public class MainActivity extends AppCompatActivity  {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_availibility:
-                    fm.beginTransaction().hide(active).show(availibilityFragment).commit();
+                    fm.beginTransaction().hide(active).commit();
                     active = availibilityFragment;
+                    fm.beginTransaction().show(availibilityFragment).commit();
+                    getSupportActionBar().setTitle(R.string.title_avilibility);
+
                     return true;
 
                 case R.id.navigation_dashboard:
-                    fm.beginTransaction().hide(active).show(dashboardFragment).commit();
+                    fm.beginTransaction().hide(active).commit();
                     active = dashboardFragment;
+                    fm.beginTransaction().show(dashboardFragment).commit();
+                    getSupportActionBar().setTitle(R.string.app_name);
                     return true;
 
                 case R.id.navigation_settings:
-                    fm.beginTransaction().hide(active).show(settingsFragment).commit();
+                    fm.beginTransaction().hide(active).commit();
                     active = settingsFragment;
+                    fm.beginTransaction().show(settingsFragment).commit();
+                    getSupportActionBar().setTitle(R.string.title_settings);
                     return true;
             }
             return false;
@@ -59,13 +66,20 @@ public class MainActivity extends AppCompatActivity  {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.getMenu().findItem(R.id.navigation_dashboard).setChecked(true);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        fm.beginTransaction().add(R.id.main_container, availibilityFragment, "3").hide(availibilityFragment).commit();
-        fm.beginTransaction().add(R.id.main_container, settingsFragment, "2").hide(settingsFragment).commit();
-        fm.beginTransaction().add(R.id.main_container, dashboardFragment, "1").commit();
+        AddFragment();
+        active = dashboardFragment;
+        fm.beginTransaction().show(dashboardFragment).commit();
+        fm.beginTransaction().hide(availibilityFragment).commit();
+        fm.beginTransaction().hide(settingsFragment).commit();
 
 
 
     }
 
+
+
+
+    public void AddFragment(){
+        fm.beginTransaction().add(R.id.main_container, dashboardFragment, "1").add(R.id.main_container, settingsFragment, "2").add(R.id.main_container, availibilityFragment, "3").commit();
+    }
 }
